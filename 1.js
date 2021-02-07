5c3276ab84464bcbb88191fe5345b7b920308933
Phạm Đức Long <long96nb@gmail.com>
2021-02-08 00:54:35 +0700

    'ok'

---
+++ 1.js
@@ -0,0 +1,27 @@
+﻿ $(function() {
+     $('.menu>li').mouseover(function() {
+         if (this.getElementsByClassName('menuchild').length > 0) {
+             this.getElementsByClassName('menuchild')[0].style.visibility = 'visible';
+             this.getElementsByClassName('menuchild')[0].style.opacity = 1;
+         }
+         if (this.getElementsByClassName('danhmuc').length > 0) {
+             this.getElementsByClassName('danhmuc')[0].style.visibility = 'visible';
+             this.getElementsByClassName('danhmuc')[0].style.opacity = 1;
+         }
+
+     });
+     $('.menu>li').mouseout(function() {
+         if (this.getElementsByClassName('menuchild').length > 0) {
+             this.getElementsByClassName('menuchild')[0].style.visibility = 'hidden';
+             this.getElementsByClassName('menuchild')[0].style.opacity = 0;
+         }
+         if (this.getElementsByClassName('danhmuc').length > 0) {
+             this.getElementsByClassName('danhmuc')[0].style.visibility = 'hidden';
+             this.getElementsByClassName('danhmuc')[0].style.opacity = 0;
+         }
+     });
+     $('#myCarousel').carousel({
+         interval: 10000
+     });
+
+ })
